b3eb0877876130d8ee7d43679f85622a68a6492e
karmakarAshis <karmakar15-4035@diu.edu.bd>
2024-02-16 11:04:24 +0600

    triangle section added

---
+++ scripts/rectangle.js
@@ -0,0 +1,19 @@
+function calculateRectangleArea(){
+    // get width from ui
+    const rectangleWidthInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+
+    // get height from ui
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+    
+
+    // calculate area
+    const area = 0.5 * base * height;
+    console.log(area);
+    // display area
+    const triangleAreaSpan = document.getElementById('display-span');
+    triangleAreaSpan.innerText = area;
+}
